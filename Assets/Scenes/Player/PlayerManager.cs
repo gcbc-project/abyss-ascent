5c7bbab6e3bfb670c699cb54cd31cd46081bf058
l11040 <113540683+l11040@users.noreply.github.com>
2024-05-29 17:15:53 +0900

    Add : PlayerManager 작성

---
+++ Assets/Scenes/Player/PlayerManager.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    private static PlayerManager _instance;
+    public PlayerManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = FindObjectOfType<PlayerManager>();
+                if (_instance == null)
+                {
+                    GameObject obj = new GameObject(typeof(PlayerManager).Name);
+                    _instance = obj.AddComponent<PlayerManager>();
+                    DontDestroyOnLoad(obj);
+                }
+            }
+            return _instance;
+        }
+    }
+
+    private void Awake()
+    {
+        if (_instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            _instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
