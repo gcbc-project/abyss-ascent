31e80fd3079b3d9bdcf1155f3ec3c3fa9692c778
l11040 <113540683+l11040@users.noreply.github.com>
2024-05-30 16:02:29 +0900

    Add : 플레이어 애니메이션 기능 추가

--- Assets/Scenes/Player/System/PlayerAnime.cs
+++ Assets/Scenes/Player/System/PlayerAnime.cs
@@ -1,6 +1,28 @@
 using UnityEngine;
 
-public class PlayerAnimation : MonoBehaviour
+public class PlayerAnime : MonoBehaviour
 {
+    private readonly int _isWalk = Animator.StringToHash("isWalk");
+    private readonly int _jump = Animator.StringToHash("jump");
 
+    private Animator _animator;
+    private PlayerJump _playerJump;
+
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
+        _playerJump = GetComponent<PlayerJump>();
+        PlayerManager.Instance.Player.Input.OnMoveInputEvent += OnMove;
+        _playerJump.OnJumpEvent += OnJump;
+    }
+
+    private void OnJump()
+    {
+        _animator.SetTrigger(_jump);
+    }
+
+    private void OnMove(Vector2 direction)
+    {
+        _animator.SetBool(_isWalk, direction.magnitude > 0f);
+    }
 }
