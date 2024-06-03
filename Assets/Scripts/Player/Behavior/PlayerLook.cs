e09ab3719e2b06263886effb326410cb543ac1b3
dhrzld <dhrzld@gmail.com>
2024-06-03 19:23:33 +0900

    moving

--- Assets/Scripts/Player/Behavior/PlayerLook.cs
+++ Assets/Scripts/Player/Behavior/PlayerLook.cs
@@ -3,34 +3,23 @@ using UnityEngine;
 public class PlayerLook : MonoBehaviour
 {
     private Camera _camera;
-    private Vector2 _currentRotation = Vector2.zero;
-    private Quaternion _cameraRotation;
+    private Vector3 _cameraOffset;
     private float _distance = 4.0f;
-    private float _mouseSpeed = 0.1f;
     private float _yOffset = 1.0f;
 
     private void Start()
     {
         _camera = Camera.main;
         Cursor.lockState = CursorLockMode.Locked;
-        PlayerManager.Instance.Player.Input.OnLookInputEvent += OnLook;
+        _cameraOffset = new Vector3(0.0f, _yOffset, -_distance);
     }
     private void LateUpdate()
     {
-        if (_cameraRotation != null)
-        {
-            Vector3 position = _cameraRotation * new Vector3(0.0f, 0.0f, -_distance) + transform.position;
-            position.y += _yOffset;
+        //마우스 카메라 x
+        Quaternion cameraRotation = Quaternion.Euler(20f, 0f, 0f);
+        Vector3 position = cameraRotation * _cameraOffset + transform.position;
 
-            _camera.transform.rotation = _cameraRotation;
-            _camera.transform.position = position;
-        }
-    }
-    private void OnLook(Vector2 mouseDelta)
-    {
-        _currentRotation.x += mouseDelta.x * _mouseSpeed;
-        _currentRotation.y -= mouseDelta.y * _mouseSpeed;
-
-        _cameraRotation = Quaternion.Euler(_currentRotation.y, _currentRotation.x, 0);
+        _camera.transform.rotation = cameraRotation;
+        _camera.transform.position = position;
     }
 }
