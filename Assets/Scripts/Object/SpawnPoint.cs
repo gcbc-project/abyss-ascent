48040f007ca0998cb76edfd04983d8b3c14a317d
l11040 <113540683+l11040@users.noreply.github.com>
2024-06-11 13:02:11 +0900

    Update

--- Assets/Scripts/Object/SpawnPoint.cs
+++ Assets/Scripts/Object/SpawnPoint.cs
@@ -2,13 +2,21 @@ using UnityEngine;
 
 public class SpawnPoint : MonoBehaviour
 {
+    LayerMask _playerLayerMask;
     public bool IsNextSet = false;
+    private void Start()
+    {
+        _playerLayerMask = LayerMask.GetMask("Player");
+    }
     private void OnTriggerEnter(Collider other)
     {
-        if (!IsNextSet)
+        if (1 << other.gameObject.layer == _playerLayerMask)
         {
-            SpwanManager.Instance.SetNextSpawn();
-            IsNextSet = true;
+            if (!IsNextSet)
+            {
+                SpwanManager.Instance.SetNextSpawn();
+                IsNextSet = true;
+            }
         }
     }
 }
